943d0d0240230f1d98447379fa8244703ad4ad14
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2024-07-06 16:29:18 +0530

    final update "cors error"

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ app.use(express.urlencoded({
 
 app.use(cookieParser());
 app.use(cors({
-    // origin:process.env.FRONTEND_URL,
-    origin:"https://course-bundler-frontend.vercel.app/",
+    origin:process.env.FRONTEND_URL,
     credentials:true,
     methods:["GET","POST","DELETE","PUT"]
 }))
